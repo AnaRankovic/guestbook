eb8b6f94d81ed0d2ea357364f71fe50fe20d703f
AnaRankovic <anarankovic11@gmail.com>
2015-08-10 18:44:36 +0200

    Chapeter 2: Login & Logout

--- guestbook/src/guestbook/handler.clj
+++ guestbook/src/guestbook/handler.clj
@@ -7,7 +7,9 @@
             [compojure.route :as route]
             [guestbook.routes.home :refer [home-routes]]
             [guestbook.models.db :as db]
-            [guestbook.routes.auth :refer [auth-routes]]))
+            [guestbook.routes.auth :refer [auth-routes]]
+            [noir.session :as session]
+            [ring.middleware.session.memory :refer [memory-store]]))
 
 (defn init []
 (println "guestbook is starting")
@@ -22,5 +24,10 @@
   (route/not-found "Not Found"))
 
 (def app
+(->
 (handler/site
-(routes auth-routes home-routes app-routes)))
+(routes auth-routes
+home-routes
+app-routes))
+(session/wrap-noir-session
+{:store (memory-store)})))
--- guestbook/src/guestbook/routes/home.clj
+++ guestbook/src/guestbook/routes/home.clj
@@ -2,7 +2,8 @@
 (:require [compojure.core :refer :all]
 [guestbook.views.layout :as layout]
 [hiccup.form :refer :all]
-[guestbook.models.db :as db]))
+[guestbook.models.db :as db]
+[noir.session :as session]))
 
 (defn show-guests []
 [:ul.guests
@@ -14,22 +15,14 @@
 
 (defn home [& [name message error]]
 (layout/common
-[:h1 "Guestbook"]
+[:h1 "Guestbook " (session/get :user)]
 [:p "Welcome to my guestbook"]
 [:p error]
-;here we call our show-guests function
-;to generate the list of existing comments
 (show-guests)
 [:hr]
-;here we create a form with text fields called "name" and "message"
-;these will be sent when the form posts to the server as keywords of
-;the same name
 (form-to [:post "/"]
-[:p "Name:"]
-(text-field "name" name)
-[:p "Message:"]
-(text-area {:rows 10 :cols 40} "message" message)
-[:br]
+[:p "Name:" (text-field "name" name)]
+[:p "Message:" (text-area {:rows 10 :cols 40} "message" message)]
 (submit-button "comment"))))
 
 (defn save-message [name message]
@@ -59,3 +52,4 @@
 (defroutes home-routes
 (GET "/" [] (home))
 (POST "/" [name message] (save-message name message)))
+
